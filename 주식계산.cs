57b47c36cde8f0411594b38c940a514e5faab1a4
YoonHyunSung <gustjd6495@gmail.com>
2024-11-25 22:01:11 +0900

    2024-11-25 스택/큐
    주식계산,프로세스

---
+++ 주식계산.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study
+{
+    partial class 주식계산
+    {
+        
+        /*static void Main()
+        {
+            int[] prices = new int[] { 1, 2, 3, 2, 3 };
+            int[] result = solution(prices);
+        }*/
+
+        private static int[] solution(int[] prices)
+        {
+            int[] answer = new int[prices.Count()];
+     
+            Stack<int> stkIndex = new Stack<int>();
+            for (int i = 0; i < prices.Length; i++)
+            {
+                while ((stkIndex.Count > 0) && prices[stkIndex.Peek()] > prices[i])
+                {
+                    int idx = stkIndex.Pop();
+                    answer[idx] = i - idx;
+                }
+                stkIndex.Push(i);
+            }
+
+
+
+
+            while (stkIndex.Count > 0)
+            {
+                int idx = stkIndex.Pop();
+                answer[idx] = prices.Length - 1 - idx;
+            }
+
+            return answer;
+        }
+    }
+}
