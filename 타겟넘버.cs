17d5daefe5b7cd10c054f10a90a00bb7479da289
YoonHyunSung <gustjd6495@gmail.com>
2024-11-28 21:30:54 +0900

    타겟넘버
    DFS

---
+++ 타겟넘버.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study
+{
+    public class 타겟넘버
+    {
+        int NumbersCount = 0;
+        int result = 0;
+        int target;
+        int[] numbers = new int[] { };
+        public void Main()
+        {
+
+            numbers = new int[] { 4, 1, 2, 1 };
+            target = 3;
+            int result = solution(numbers, target);
+        }
+
+        private int cac(int i, int sum) {
+
+            if (i == numbers.Count())
+                return sum == target ? 1 : 0;
+
+            
+            int add = cac(i + 1, sum + numbers[i]);
+            int subtract = cac(i + 1, sum - numbers[i]);
+
+         
+            return add + subtract;
+            }
+
+        public int solution(int[] Pnumbers, int Ptarget)
+        {
+            numbers = Pnumbers;
+            target = Ptarget;
+            
+            
+
+
+
+            return cac(0, 0);
+        }
+    }
+}
