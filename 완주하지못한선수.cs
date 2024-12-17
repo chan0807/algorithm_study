45d3632d0b9fb16d87c610d7db2503049021835b
YoonHyunSung <gustjd6495@gmail.com>
2024-12-17 20:58:08 +0900

    2024-12-17
    2024-12-17

---
+++ 완주하지못한선수.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study
+{
+    class 완주하지못한선수
+    {
+        /*public static void Main()
+        {
+            //string[] participant = new string[] { "leo", "kiki", "eden" };
+            //string[] completion = new string[] { "eden", "kiki" };
+            //string[] participant = new string[] { "marina", "josipa", "nikola", "vinko", "filipa" };
+            //string[] completion =  new string[]  {"josipa", "filipa", "marina", "nikola" };
+            string[] participant = new string[] {"mislav", "stanko", "mislav", "ana"};
+            string[] completion = new string[] {"stanko", "ana", "mislav"};
+
+            string result = solution(participant, completion);
+        }*/
+        public static String solution(String[] participant, String[] completion)
+        {
+            List<string> parList = new List<string>(participant);
+
+            foreach (string item in completion)
+            {
+                parList.Remove(item);
+            }
+
+            return string.IsNullOrEmpty(parList.FirstOrDefault()) ? string.Empty : parList.FirstOrDefault();
+        }
+    }
+}
