49e62c88169c082a9599a4c9fb6cafb34e992485
a-rin2 <34049818+a-rin2@users.noreply.github.com>
2025-01-16 22:09:52 +0900

    5주차 - (1)

---
+++ 모의고사.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Study
+{
+    internal class 모의고사
+    {
+        public static int[] solution(int[] answers)
+        {
+            int[] answer = new int[] { };
+            List<int> answerList = new List<int>();
+
+            Dictionary<int, int[]> students = new Dictionary<int, int[]>();
+
+            int[] temp1 = { 1, 2, 3, 4, 5 };
+            int[] temp2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
+            int[] temp3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+
+            //students.Add(1, [1, 2, 3, 4, 5]);
+            //students.Add(2, [2, 1, 2, 3, 2, 4, 2, 5]);
+            //students.Add(3, [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]);
+            students.Add(1, temp1);
+            students.Add(2, temp2);
+            students.Add(3, temp3);
+
+            Dictionary<int, int> studentScore = new Dictionary<int, int>();            
+
+            studentScore.Add(1, 0);
+            studentScore.Add(2, 0);
+            studentScore.Add(3, 0);
+
+            foreach (KeyValuePair<int, int[]> student in students)
+            {
+                int name = student.Key;
+                int[] value = student.Value;
+
+                int score = 0;
+
+                for (int i = 0; i < answers.Length; i++)
+                {
+                    if (answers[i] == value[i % value.Length])
+                        score++;
+                }
+
+                studentScore[name] = score;
+            }
+
+            // 정렬
+            var scoreSort = studentScore.OrderByDescending(x => x.Value);
+
+            //var firstName = studentScore.OrderByDescending(x => x.Value).First().Key;
+            var max = studentScore.OrderByDescending(x => x.Value).First().Value;
+
+            foreach (KeyValuePair<int, int> score in scoreSort)
+            {
+                if (answerList.Count == 0)
+                {
+                    answerList.Add(score.Key);
+                }
+                else
+                {
+                    if (score.Value == max)
+                        answerList.Add(score.Key);
+                    else
+                        break;
+                }                
+            }
+
+            answer = answerList.ToArray();            
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            int[] answers = [2, 1, 2, 3, 2, 1, 2];// [1, 3, 2, 4, 2];//[1, 2, 3, 4, 5];
+
+            string[] result = solution(answers).Select(i => i.ToString()).ToArray();
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
---
+++ 최소직사각형.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Study
+{
+    internal class 최소직사각형
+    {
+        public static int solution(int[,] sizes)
+        {
+            int answer = 0;
+
+            int length = sizes.GetLength(0);
+
+            int[] largeNo = new int[length];
+            int[] smallNo = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                if (sizes[i, 0] > sizes[i, 1])
+                {
+                    largeNo[i] = sizes[i, 0];
+                    smallNo[i] = sizes[i, 1];
+                }
+                else
+                {
+                    largeNo[i] = sizes[i, 1];
+                    smallNo[i] = sizes[i, 0];
+                }
+            }
+
+            Array.Sort(largeNo);
+            Array.Sort(smallNo);
+
+            answer = largeNo[length-1] * smallNo[length - 1];
+
+            return answer;
+        }
+
+        //static void Main()
+        //{
+        //    int[,] sizes = { { 60, 50 }, { 30, 70 }, { 60, 30 }, { 80, 40 } };
+
+        //    int result = solution(sizes);
+
+        //    Console.WriteLine(result);
+        //}
+    }
+}
